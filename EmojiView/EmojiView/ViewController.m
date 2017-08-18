b351cc88203639ecff3a1366b8863fc063dacd91
Viachaslau Kastsechka <viacheslav.kostechko@gmail.com>
2017-08-18 17:49:14 +0300

    build fix. Setup dev project

--- EmojiView/EmojiView/ViewController.m
+++ EmojiView/EmojiView/ViewController.m
@@ -7,23 +7,40 @@
 //
 
 #import "ViewController.h"
+#import "ISEmojiView.h"
 
-@interface ViewController ()
+@interface ViewController () <ISEmojiViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIView *emojiContainerView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self addEmojiView];
 }
 
+#pragma mark - ISEmojiViewDelegate
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)emojiView:(nonnull ISEmojiView *)emojiView didSelectEmoji:(nonnull NSString *)emoji {
+    [self.textView insertText:emoji];
 }
 
+- (void)emojiView:(nonnull ISEmojiView *)emojiView didPressDeleteButton:(nonnull UIButton *)deleteButton {
+    [self.textView deleteBackward];
+}
+
+#pragma mark - Private
+
+- (void)addEmojiView {
+    ISEmojiView *emojiView = [[ISEmojiView alloc] initWithFrame:self.emojiContainerView.bounds];
+    emojiView.delegate = self;
+    [self.emojiContainerView addSubview:emojiView];
+}
 
 @end
